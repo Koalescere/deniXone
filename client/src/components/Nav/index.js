2d862f0f54f308f3ad882c7a8470ce6d2c96b0d4
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-12-12 23:40:18 -0500

    commiting early and often, starting to build the navbar and set up a form, footer and map folders and js, css files within the components.  Also added an images folder to the public folder

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,41 @@
 import React from "react";
+import "./navbar.css";
 
 function Nav() {
   return (
+    
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+         <a className="navbar-brand" href="/"> 
+              deniXone
+         </a>
+
+         <div class="col" id="righttitle">
+        <div class="dropdown" id="accountdiv">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user-alt"></i>
+            </a>
+          
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Account</a>
+              <a class="dropdown-item" href="#">Favorites</a>
+              <a class="dropdown-item" href="#">Log out</a>
+            </div>
+          </div>
+    </div>
+
+     </nav>
   );
 }
 
 export default Nav;
+
+
+
+
+
+
+// <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <a className="navbar-brand" href="/">
+//         React Reading List
+//       </a>
+//     </nav>
