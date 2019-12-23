46127adc37cfef65557c194011db75c84648547d
Test123 <Test123@gavins-mbp.home>
2019-12-22 23:26:29 -0500

    push authenticate to repo

--- client/src/components/Welcomejumbo/index.js
+++ client/src/components/Welcomejumbo/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Welcomejumbo.css";
 
+
 function Welcomejumbo() {
   return (
     <div className="jumbotron">
@@ -9,11 +10,11 @@ function Welcomejumbo() {
           DENI<strong className="cross">X</strong>ONE
         </h1>
         <p className="lead">
-        DENIXONE is a visual tool designed to show trends in property values.  
+          DENIXONE is a visual tool designed to show trends in property values.
         </p>
         {/* <hr className="my-4" /> */}
         <p className="follows">
-         Built upon our database of current and historical real estate sales. DENIXONE provides   
+          Built upon our database of current and historical real estate sales. DENIXONE provides
         </p>
         <p className="follows">
           <a className="btn btn-primary btn-lg" href="./books" role="button">
