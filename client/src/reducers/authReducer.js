ab04b9a3100275fe87383df3e5e521be3d9cf37c
Test123 <Test123@Gavins-MacBook-Pro.local>
2020-01-11 12:07:27 -0500

    removed react-mapbox-components

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -15,7 +15,7 @@ const initialState = {
   isLoading: false,
   user: null
 };
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case USER_LOADING:
       return {
