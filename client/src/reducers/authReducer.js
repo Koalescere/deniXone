eebde59bfe6ea0da35176474071581dd5faa888e
Kayode Oseni <k.oseni.jr@gmail.com>
2020-02-14 12:37:39 -0500

    testing

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -16,7 +16,6 @@ const initialState = {
   user: null
 };
 export default function (state = initialState, action) {
-<<<<<<< HEAD
     switch (action.type) {
         case USER_LOADING:
             return {
@@ -54,43 +53,4 @@ export default function (state = initialState, action) {
         default:
             return state;
     }
-=======
-  switch (action.type) {
-    case USER_LOADING:
-      return {
-        ...state,
-        isLoading: true
-      };
-    case USER_LOADED:
-      return {
-        ...state,
-        isAuthenticated: true,
-        isLoading: false,
-        user: action.payload
-      };
-    case LOGIN_SUCCESS:
-    case REGISTER_SUCCESS:
-      localStorage.setItem("token", action.payload.token);
-      return {
-        ...state,
-        ...action.payload,
-        isAuthenticated: true,
-        isLoading: false
-      };
-    case AUTH_ERROR:
-    case LOGIN_FAIL:
-    case LOGOUT_SUCCESS:
-    case REGISTER_FAIL:
-      localStorage.removeItem("token");
-      return {
-        ...state,
-        token: null,
-        user: null,
-        isAuthenticated: false,
-        isLoading: false
-      };
-    default:
-      return state;
-  }
->>>>>>> 8de052339f3fcadeb857839682ad35db1313b502
 }
